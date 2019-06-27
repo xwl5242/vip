45003e2b79e3e08de3729fb5b36520174c5ca1fb
xwl5242 <xwl5242@163.com>
2019-06-27 16:38:43 +0800

    news

--- dao.py
+++ dao.py
@@ -9,11 +9,15 @@ class DB:
                          charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
     @staticmethod
-    def query_news():
-        sql = "select id, tv_id, tv_name, tv_img, tv_detail, update_time from t_tv order by update_time desc limit 0,12"
+    def query_index_news():
+        sql = "select id, tv_id, tv_name, tv_img, tv_actors, update_time from t_tv order by update_time desc limit 0,12"
         cursor = DB.db.cursor()
         cursor.execute(sql)
         return cursor.fetchall()
+
+    @staticmethod
+    def query_index_mv():
+        sql = ""
 
 
 
