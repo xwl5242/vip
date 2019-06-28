88930492b140bd9051e5333cab30eadfce2e3e9d
xwl5242 <xwl5242@163.com>
2019-06-28 12:31:14 +0800

    index & detial

--- dao.py
+++ dao.py
@@ -4,21 +4,55 @@ import configparser
 
 
 class Config:
+    """
+    全局配置类
+    """
+    # 读取全局配置文件config.ini
     cf = configparser.ConfigParser()
     cf.read('config.ini', encoding='utf-8')
+    # 视频类型section
     MV = [mv for mv in cf.get('tv_type', 'mv').split(',')]
     DM = [dm for dm in cf.get('tv_type', 'dm').split(',')]
     ZY = [zy for zy in cf.get('tv_type', 'zy').split(',')]
     DSJ = [dsj for dsj in cf.get('tv_type', 'dsj').split(',')]
+    MV_TYPE = [(mv.split(':')[0], mv.split(':')[1]) for mv in cf.get('tv_type', 'mv_type').split(',')]
+    DM_TYPE = [(dm.split(':')[0], dm.split(':')[1]) for dm in cf.get('tv_type', 'dm_type').split(',')]
+    ZY_TYPE = [(zy.split(':')[0], zy.split(':')[1]) for zy in cf.get('tv_type', 'zy_type').split(',')]
+    DSJ_TYPE = [(dsj.split(':')[0], dsj.split(':')[1]) for dsj in cf.get('tv_type', 'dsj_type').split(',')]
+    TYPES = {'mv': MV_TYPE, 'dm': DM_TYPE, 'zy': ZY_TYPE, 'dsj': DSJ_TYPE}
 
 
 class DB:
+    """
+    数据库操作类
+    """
     db = pymysql.Connect(host='182.61.172.229', user='root',
                          password='@xwl082933048.', database='tv',
                          charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
     @staticmethod
+    def query_index_today_total_update():
+        """
+        首页今日更新和总视频数量
+        :return:
+        """
+        today_sql = "select count(1) total from t_tv where date_format(update_time,'%Y-%m-%d')=date_format(now(),'%Y-%m-%d') "
+        total_sql = "select count(1) total from t_tv"
+        cursor = DB.db.cursor()
+        cursor.execute(today_sql)
+        today = cursor.fetchone()
+        cursor.execute(total_sql)
+        total = cursor.fetchone()
+        today = today['total'] if today and today['total'] else 0
+        total = total['total'] if total and total['total'] else 0
+        return today, total
+
+    @staticmethod
     def query_index_news():
+        """
+        首页最新视频
+        :return:
+        """
         sql = "select id, tv_id, tv_name, tv_img, tv_actors, tv_type, update_time " \
               "from t_tv order by update_time desc limit 0,12"
         cursor = DB.db.cursor()
@@ -27,6 +61,11 @@ class DB:
 
     @staticmethod
     def query_index_mvs(config_tv_type):
+        """
+        首页各种类视频
+        :param config_tv_type:
+        :return:
+        """
         where_str = "','".join(config_tv_type)
         sql = f"select id, tv_id, tv_name, tv_img, tv_actors, tv_type, tv_area, update_time " \
             f"from t_tv where tv_type in('{where_str}') order by update_time desc limit 8"
@@ -34,7 +73,31 @@ class DB:
         cursor.execute(sql)
         return cursor.fetchall()
 
+    @staticmethod
+    def query_tv_detail(tv_id):
+        d_sql = f"select tv.tv_id, tv_img, tv_name, tv_actors, tv_director, tv_type, tv_area, tv_lang, " \
+                f"tv_year, tv_intro, tv_remark, update_time from t_tv tv where tv.tv_id='{tv_id}'"
+        u_sql = f"select tv_url from t_tv_urls where tv_id='{tv_id}'"
+        cursor = DB.db.cursor()
+        cursor.execute(d_sql)
+        detail = cursor.fetchone()
+        if detail:
+            cursor.execute(u_sql)
+            urls = cursor.fetchall()
+            urls = [url['tv_url'] for url in urls]
+            detail['urls'] = urls
+        return detail
 
+    @staticmethod
+    def query_friend_urls():
+        """
+        首页友情链接
+        :return:
+        """
+        sql = "select f_title,f_url from t_f_u where del_flag='0'"
+        cursor = DB.db.cursor()
+        cursor.execute(sql)
+        return cursor.fetchall()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import base64
 from flask import Flask, render_template, jsonify
 from dao import DB, Config
 # name, static resource path, templates resource path
@@ -7,22 +8,86 @@ app = Flask("yo_vip_tv", static_folder="static", template_folder="templates")
 
 @app.route('/')
 def index():
+    """
+    网站首页
+    :return:
+    """
+    # 今日更新和总视频数量
+    today, total = DB.query_index_today_total_update()
+    # news:最新视频,mvs:电影,dsjs:电视剧,dms:动漫,zys:综艺,today:今日更新,total:总视频,
+    # mv_type:电影类型,dm_type:动漫类型,zy_type:综艺类型,dsj_type:电视剧类型,fus:友情链接
     return render_template('index.html', news=DB.query_index_news(), mvs=DB.query_index_mvs(Config.MV),
                            dsjs=DB.query_index_mvs(Config.DSJ), dms=DB.query_index_mvs(Config.DM),
-                           zys=DB.query_index_mvs(Config.ZY))
+                           zys=DB.query_index_mvs(Config.ZY), today=today, total=total, mv_type=Config.MV_TYPE,
+                           dm_type=Config.DM_TYPE, zy_type=Config.ZY_TYPE, dsj_type=Config.DSJ_TYPE,
+                           fus=DB.query_friend_urls())
 
 
-@app.route('/tv')
-def tv():
+@app.route('/t-d/<tv_id>')
+def tv_detail(tv_id):
+    """
+    tv视频详情页
+    :param tv_id:
+    :return:
+    """
+    return render_template('tv/tv_detail.html', tv_detail=DB.query_tv_detail(tv_id))
+
+
+@app.route('/t-t/<tv_type>-<tv_index>')
+def tv_type(tv_type, tv_index):
+    """
+    tv视频某一类详情页
+    :param tv_id:
+    :return:
+    """
     return render_template('tv/tv.html')
 
 
-@app.route('/news')
-def news():
-    return jsonify(DB.query_index_news())
+@app.route('/tv/more/<tv_type>')
+def tv_more(tv_type):
+    """
+    tv视频更多页
+    :param tv_type:
+    :return:
+    """
+    # tv_type: all/mv/dm/zy/dsj
+    return render_template('tv/tv.html')
+
+
+@app.route('/t-play/url=<url>')
+def tv_play_by_url(url):
+    url = str(base64.b64decode(str(url).encode('utf-8')), 'utf-8')
+    print(url)
+
+
+def split_strings(s, sep=','):
+    s = s if s else ''
+    return str(s).split(sep)
+
+
+def tv_is_mv(t_type):
+    t_type = t_type if t_type else 'none'
+    return '1' if t_type in Config.MV else '0'
+
+
+def get_list(l, index):
+    return l[index] if l and len(l) > 0 else []
+
+
+def get_sub_list(l, start, end):
+    return l[start:end] if l and len(l) > 0 else []
+
+
+def b64encode(s):
+    return str(base64.b64encode(s.encode('utf-8')), 'utf-8')
 
 
 if __name__ == '__main__':
     # DEBUG RUN
+    app.add_template_filter(split_strings, 'str_split')
+    app.add_template_filter(tv_is_mv, 'tv_is_mv')
+    app.add_template_filter(get_list, 'get_list')
+    app.add_template_filter(get_sub_list, 'get_sub_list')
+    app.add_template_filter(b64encode, 'b64encode')
     app.run(port=80, debug=True)
 
