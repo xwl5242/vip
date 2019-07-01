fd5b1173327509bba3a3ac2e5d0f9e6932e20dce
xwl5242 <xwl5242@163.com>
2019-07-01 17:37:51 +0800

    重构

--- app/db/base.py
+++ app/db/base.py
@@ -25,9 +25,9 @@ class Base:
     @staticmethod
     @app_db
     def query_page(cursor, select_sql, page_no):
-        page_no = 1 if page_no or page_no == '0' or page_no == 0 else int(page_no)
+        page_no = 1 if not page_no or page_no == '0' or page_no == 0 else int(page_no)
         select_count_sql = "select count(1) total "+select_sql[select_sql.index('from'):]
-        select_sql = select_sql + f' limit {(page_no-1)*30},{page_no*30}'
+        select_sql = select_sql + f' limit {(page_no-1)*30},30'
         cursor.execute(select_count_sql)
         total = cursor.fetchone()['total']
         cursor.execute(select_sql)
